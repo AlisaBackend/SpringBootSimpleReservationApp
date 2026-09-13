15ddf70b4c54b70e4cae894cf4c2bf2c272f3cc9
AlisaBackend <aspelpal7@gmail.com>
2026-09-13 20:14:47 +0300

    add List Reservation

--- src/main/java/com/example/demoSpringBootFundamentals/ReservationController.java
+++ src/main/java/com/example/demoSpringBootFundamentals/ReservationController.java
@@ -1,5 +1,7 @@
 package com.example.demoSpringBootFundamentals;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +15,21 @@ public class ReservationController {
         this.reservationService = reservationService;
     }
 
+    //http://localhost:8080/11  -например
     @GetMapping("/{id}")
     public Reservation getReservationById(
        @PathVariable("id") Long id
     ) {
         System.out.println("lod called method");
         return reservationService.getReservationById(id);
+    }
+
+    //Метод для выведения всех бронирований
+    //http://localhost:8080/
+    @GetMapping()
+    public List<Reservation> getAllReservations() {
+        System.out.println("log called getAllReservations method");
+        return reservationService.findAllReservation();
     }
 
     
--- src/main/java/com/example/demoSpringBootFundamentals/ReservationService.java
+++ src/main/java/com/example/demoSpringBootFundamentals/ReservationService.java
@@ -1,6 +1,7 @@
 package com.example.demoSpringBootFundamentals;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,27 @@ public class ReservationService {
             LocalDate.now(),
             LocalDate.now().plusDays(5),
             ReservationStatus.APPROVED
+        );
+    }
+    //метод который отдаст список брнирований
+    public List<Reservation> findAllReservation() {
+        return List.of(
+            new Reservation(
+                1L,
+                100L,
+                40L,
+                LocalDate.now(),
+                LocalDate.now().plusDays(5),
+                ReservationStatus.APPROVED
+                ),
+            new Reservation(
+                2L,
+                100L,
+                40L,
+                LocalDate.now(),
+                LocalDate.now().plusDays(5),
+                ReservationStatus.APPROVED
+                )
         );
     }
 }
