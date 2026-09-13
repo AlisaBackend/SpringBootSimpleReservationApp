e74e141759842eb3fd707cbb42b670e7bcafc3d0
AlisaBackend <aspelpal7@gmail.com>
2026-09-13 20:27:24 +0300

    add Map and change findAllReservation()

--- src/main/java/com/example/demoSpringBootFundamentals/ReservationService.java
+++ src/main/java/com/example/demoSpringBootFundamentals/ReservationService.java
@@ -2,11 +2,44 @@ package com.example.demoSpringBootFundamentals;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 import org.springframework.stereotype.Service;
 
 @Service 
 public class ReservationService {
+
+
+
+    //Map для хранения Reservation по id
+    private final Map<Long, Reservation> reservationMap = Map.of(
+        1L, new Reservation(
+            1L,
+            100L,
+            40L,
+            LocalDate.now(),
+            LocalDate.now().plusDays(5),
+            ReservationStatus.APPROVED
+        ),
+        2L, new Reservation(
+            2L,
+            101L,
+            41L,
+            LocalDate.now(),
+            LocalDate.now().plusDays(5),
+            ReservationStatus.APPROVED
+        ),
+        3L, new Reservation(
+            3L,
+            102L,
+            42L,
+            LocalDate.now(),
+            LocalDate.now().plusDays(5),
+            ReservationStatus.APPROVED
+        )
+    );
+
     public Reservation getReservationById(Long id) {
         return new Reservation(
             id,
@@ -17,25 +50,18 @@ public class ReservationService {
             ReservationStatus.APPROVED
         );
     }
-    //метод который отдаст список брнирований
+
+    //метод для проверки 
+    public Reservation getReservationById(Long id){
+        if (reservationMap.containsKey(id)){
+            throw new NoSuchElementException("Not found resesrvation by id = " + id);
+        }
+        return reservationMap.get(id);
+    }
+
+
+    //метод который отдаст список бронирований
     public List<Reservation> findAllReservation() {
-        return List.of(
-            new Reservation(
-                1L,
-                100L,
-                40L,
-                LocalDate.now(),
-                LocalDate.now().plusDays(5),
-                ReservationStatus.APPROVED
-                ),
-            new Reservation(
-                2L,
-                100L,
-                40L,
-                LocalDate.now(),
-                LocalDate.now().plusDays(5),
-                ReservationStatus.APPROVED
-                )
-        );
+        return reservationMap.values().stream().toList();
     }
 }
