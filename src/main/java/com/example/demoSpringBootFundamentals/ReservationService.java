fe7579f750bbba21d8386fc3d813abb5e5ca40f0
AlisaBackend <aspelpal7@gmail.com>
2026-09-13 19:32:52 +0300

    add Controller and Service

---
+++ src/main/java/com/example/demoSpringBootFundamentals/ReservationService.java
@@ -0,0 +1,11 @@
+package com.example.demoSpringBootFundamentals;
+
+import org.springframework.stereotype.Service;
+
+@Service 
+public class ReservationService {
+    public String getReservationById() {
+        return "reservation-1";
+    }
+    
+}
